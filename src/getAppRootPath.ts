1e851c5fc13a4419edf96e72d04ed8becfff79f6
milahu <milahu@gmail.com>
2022-10-24 13:47:51 +0200

    fix replace backslashes to slashes

--- src/getAppRootPath.ts
+++ src/getAppRootPath.ts
@@ -3,9 +3,9 @@ import { existsSync } from "fs-extra"
 import { realpathCwd } from "./realpathCwd"
 
 export const getAppRootPath = (): string => {
-  let cwd = realpathCwd().replace("\\", "/")
+  let cwd = realpathCwd().replace(/\\/g, "/")
   while (!existsSync(join(cwd, "package.json"))) {
-    const up = resolve(cwd, "../").replace("\\", "/")
+    const up = resolve(cwd, "../").replace(/\\/g, "/")
     if (up === cwd) {
       throw new Error("no package.json found for this project")
     }
